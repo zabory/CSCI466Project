161dc7f5aab48e5ea1da5cc063f4d26d9dad3c94
Xinyi Ye <13651362897@163.com>
2021-11-23 19:07:30 -0600

    add HUMAN, ITEM and STORE data

---
+++ INSERT.sql
@@ -0,0 +1,106 @@
+INSERT  INTO    HUMAN
+VALUE
+        ('12345678','Isabella','Lee','Williams','6505130514','z1909177@students.niu.edu'),
+        ('13792610','Jayden','','Brown','7792703338','jd1997@gmail.com'),
+        ('10129832','Ava','Louise','Johnson','8302123892','alskjdi@gmail.com'),
+        ('92830171','Stacy','Claire','Anderson','1209382307','StacyCA@yahoo.com'),
+        ('12930839','Michael','Kai','Taylor','7791209239','68213@yahoo.com');
+
+INSERT  INTO    ITEM
+VALUE
+        # FIRST ITEM WITH 4 TYPES (NEW, PRE-OWNED, DIGITAL AND WITH 1 TYPE OF PLUS EXPANSION PASS)
+        ('00000001','The Legend of Zelda: Breath of the Wild', '39.99', 'STANDARD', 'NEW', 
+        'Forget everything you know about The Legend of Zelda games. Step into a world of discovery, exploration, and adventure in The Legend of Zelda: Breath of the Wild, a boundary-breaking new game in the acclaimed series. Travel across vast fields, through forests, and to mountain peaks as you discover what has become of the kingdom of Hyrule in this stunning Open-Air Adventure. Now on Nintendo Switch, your journey is freer and more open than ever. Take your system anywhere, and adventure as Link any way you like.'
+        ),
+        ('00000002','The Legend of Zelda: Breath of the Wild', '37.99', 'STANDARD', 'PRE-OWNED', 
+        'Forget everything you know about The Legend of Zelda games. Step into a world of discovery, exploration, and adventure in The Legend of Zelda: Breath of the Wild, a boundary-breaking new game in the acclaimed series. Travel across vast fields, through forests, and to mountain peaks as you discover what has become of the kingdom of Hyrule in this stunning Open-Air Adventure. Now on Nintendo Switch, your journey is freer and more open than ever. Take your system anywhere, and adventure as Link any way you like.'
+        ),
+        ('00000003','The Legend of Zelda: Breath of the Wild', '39.99', 'STANDARD', 'DIGITAL', 
+        'Forget everything you know about The Legend of Zelda games. Step into a world of discovery, exploration, and adventure in The Legend of Zelda: Breath of the Wild, a boundary-breaking new game in the acclaimed series. Travel across vast fields, through forests, and to mountain peaks as you discover what has become of the kingdom of Hyrule in this stunning Open-Air Adventure. Now on Nintendo Switch, your journey is freer and more open than ever. Take your system anywhere, and adventure as Link any way you like.'
+        ),
+        ('00000004','The Legend of Zelda: Breath of the Wild', '79.98', 'PLUS EXPANSION PASS', 'DIGITAL', 
+        'Forget everything you know about The Legend of Zelda games. Step into a world of discovery, exploration, and adventure in The Legend of Zelda: Breath of the Wild, a boundary-breaking new game in the acclaimed series. Travel across vast fields, through forests, and to mountain peaks as you discover what has become of the kingdom of Hyrule in this stunning Open-Air Adventure. Now on Nintendo Switch, your journey is freer and more open than ever. Take your system anywhere, and adventure as Link any way you like.'
+        ),
+
+        # SECOND ITEM WITH 3 TYPES (NEW, PRE-OWNED, DIGITAL)
+        ('00000005','New Super Mario Bros U Deluxe', '39.99', 'STANDARD', 'NEW', 
+        'Join Mario, Luigi, and pals for single-player or multiplayer fun anytime, anywhere! Take on two family-friendly, side-scrolling adventures with up to three friends* as you try to save the Mushroom Kingdom. Includes the New Super Mario Bros. U game and harder, faster New Super Luigi U game—both of which include Nabbit and Toadette as playable characters!'
+        ),
+        ('00000006','New Super Mario Bros U Deluxe', '37.99', 'STANDARD', 'PRE-OWNED', 
+        'Join Mario, Luigi, and pals for single-player or multiplayer fun anytime, anywhere! Take on two family-friendly, side-scrolling adventures with up to three friends* as you try to save the Mushroom Kingdom. Includes the New Super Mario Bros. U game and harder, faster New Super Luigi U game—both of which include Nabbit and Toadette as playable characters!'
+        ),
+        ('00000007','New Super Mario Bros U Deluxe', '39.99', 'STANDARD', 'DIGITAL', 
+        'Join Mario, Luigi, and pals for single-player or multiplayer fun anytime, anywhere! Take on two family-friendly, side-scrolling adventures with up to three friends* as you try to save the Mushroom Kingdom. Includes the New Super Mario Bros. U game and harder, faster New Super Luigi U game—both of which include Nabbit and Toadette as playable characters!'
+        ),
+
+        # THIRD ITEM WITH 3 TYPES (NEW, PRE-OWNED, DIGITAL)
+        ('00000008','Kirby Star Allies', '39.99', 'STANDARD', 'NEW', 
+        'When a new evil threatens Planet Popstar, Kirby will need a little help from his…enemies?! By making friends out of foes, up to three* players can drop in or out of the adventure at any time. With new and expanded copy abilities, classic Kirby action is deeper than ever: combine abilities with elements such as ice or fire to create new friend abilities! With tons of bosses and enemies standing in your way, Kirby has a new bag of tricks. Take baddies out by taking advantage of their elemental weaknesses.'
+        ),
+        ('00000009','Kirby Star Allies', '37.99', 'STANDARD', 'PRE-OWNED', 
+        'When a new evil threatens Planet Popstar, Kirby will need a little help from his…enemies?! By making friends out of foes, up to three* players can drop in or out of the adventure at any time. With new and expanded copy abilities, classic Kirby action is deeper than ever: combine abilities with elements such as ice or fire to create new friend abilities! With tons of bosses and enemies standing in your way, Kirby has a new bag of tricks. Take baddies out by taking advantage of their elemental weaknesses.'
+        ),
+        ('00000010','Kirby Star Allies', '39.99', 'STANDARD', 'DIGITAL', 
+        'When a new evil threatens Planet Popstar, Kirby will need a little help from his…enemies?! By making friends out of foes, up to three* players can drop in or out of the adventure at any time. With new and expanded copy abilities, classic Kirby action is deeper than ever: combine abilities with elements such as ice or fire to create new friend abilities! With tons of bosses and enemies standing in your way, Kirby has a new bag of tricks. Take baddies out by taking advantage of their elemental weaknesses.'
+        ),
+
+        # 4TH ITEM WITH 3 TYPES (NEW, PRE-OWNED, DIGITAL)
+        ('00000011','Fire Emblem: Three Houses', '39.99', 'STANDARD', 'NEW', 
+        'Three territories. Three houses. Your very own journey. War is coming to the continent of Fódlan. Here, order is maintained by the Church of Seiros, which hosts the prestigious Officer’s Academy within its headquarters. You are invited to teach one of its three mighty houses, each comprised of students brimming with personality and represented by a royal from one of three territories. As their professor, you must lead your students in their academic lives and in turn-based, tactical RPG battles wrought with strategic, new twists to overcome. Which house, and which path, will you choose? Nintendo Switch Online membership (sold separately) and Nintendo Account required for online play. Not available in all countries. Internet access required for online features. Terms apply. nintendo.com/switch-online ©2019 Nintendo / INTELLIGENT SYSTEMS. Co-developed by KOEI TECMO GAMES CO., LTD. Fire Emblem and Nintendo Switch are trademarks of Nintendo. © 2019 Nintendo.'
+        ),
+        ('00000012','Fire Emblem: Three Houses', '37.99', 'STANDARD', 'PRE-OWNED', 
+        'Three territories. Three houses. Your very own journey. War is coming to the continent of Fódlan. Here, order is maintained by the Church of Seiros, which hosts the prestigious Officer’s Academy within its headquarters. You are invited to teach one of its three mighty houses, each comprised of students brimming with personality and represented by a royal from one of three territories. As their professor, you must lead your students in their academic lives and in turn-based, tactical RPG battles wrought with strategic, new twists to overcome. Which house, and which path, will you choose? Nintendo Switch Online membership (sold separately) and Nintendo Account required for online play. Not available in all countries. Internet access required for online features. Terms apply. nintendo.com/switch-online ©2019 Nintendo / INTELLIGENT SYSTEMS. Co-developed by KOEI TECMO GAMES CO., LTD. Fire Emblem and Nintendo Switch are trademarks of Nintendo. © 2019 Nintendo.'
+        ),
+        ('00000013','Fire Emblem: Three Houses', '39.99', 'STANDARD', 'DIGITAL', 
+        'Three territories. Three houses. Your very own journey. War is coming to the continent of Fódlan. Here, order is maintained by the Church of Seiros, which hosts the prestigious Officer’s Academy within its headquarters. You are invited to teach one of its three mighty houses, each comprised of students brimming with personality and represented by a royal from one of three territories. As their professor, you must lead your students in their academic lives and in turn-based, tactical RPG battles wrought with strategic, new twists to overcome. Which house, and which path, will you choose? Nintendo Switch Online membership (sold separately) and Nintendo Account required for online play. Not available in all countries. Internet access required for online features. Terms apply. nintendo.com/switch-online ©2019 Nintendo / INTELLIGENT SYSTEMS. Co-developed by KOEI TECMO GAMES CO., LTD. Fire Emblem and Nintendo Switch are trademarks of Nintendo. © 2019 Nintendo.'
+        ),
+        ('00000014','Fire Emblem: Three Houses', '84.98', 'PLUS EXPANSION PASS', 'DIGITAL', 
+        'Three territories. Three houses. Your very own journey. War is coming to the continent of Fódlan. Here, order is maintained by the Church of Seiros, which hosts the prestigious Officer’s Academy within its headquarters. You are invited to teach one of its three mighty houses, each comprised of students brimming with personality and represented by a royal from one of three territories. As their professor, you must lead your students in their academic lives and in turn-based, tactical RPG battles wrought with strategic, new twists to overcome. Which house, and which path, will you choose? Nintendo Switch Online membership (sold separately) and Nintendo Account required for online play. Not available in all countries. Internet access required for online features. Terms apply. nintendo.com/switch-online ©2019 Nintendo / INTELLIGENT SYSTEMS. Co-developed by KOEI TECMO GAMES CO., LTD. Fire Emblem and Nintendo Switch are trademarks of Nintendo. © 2019 Nintendo.'
+        ),
+
+        # 4TH ITEM WITH 3 TYPES (NEW, PRE-OWNED, DIGITAL)
+        ('00000015','Xenoblade Chronicles: Definitive Edition', '39.99', 'STANDARD', 'NEW', 
+        'Discover the origins of Shulk as he and his companions clash against a seemingly-unstoppable mechanical menace. Wield a future-seeing blade, chain together attacks, and carefully position your party members in strategic, real-time combat as you journey across a massive world. Xenoblade Chronicles: Future Connected takes place one year after the main story and delves deeper into the relationship between Shulk and Melia in the face of a mysterious new threat.'
+        ),
+        ('00000016','Xenoblade Chronicles: Definitive Edition', '37.99', 'STANDARD', 'PRE-OWNED',
+        'Discover the origins of Shulk as he and his companions clash against a seemingly-unstoppable mechanical menace. Wield a future-seeing blade, chain together attacks, and carefully position your party members in strategic, real-time combat as you journey across a massive world. Xenoblade Chronicles: Future Connected takes place one year after the main story and delves deeper into the relationship between Shulk and Melia in the face of a mysterious new threat.'
+        ),
+        ('00000017','Xenoblade Chronicles: Definitive Edition', '39.99', 'STANDARD', 'DIGITAL',
+        'Discover the origins of Shulk as he and his companions clash against a seemingly-unstoppable mechanical menace. Wield a future-seeing blade, chain together attacks, and carefully position your party members in strategic, real-time combat as you journey across a massive world. Xenoblade Chronicles: Future Connected takes place one year after the main story and delves deeper into the relationship between Shulk and Melia in the face of a mysterious new threat.'
+        ),
+
+        # 5TH ITEM WITH 3 TYPES (NEW, PRE-OWNED, DIGITAL)
+        ('00000018','Splatoon 2', '39.99', 'STANDARD', 'NEW', 
+        'Ink-splatting action is back and fresher than ever The squid kids called Inklings are back to splat more ink and claim more turf in this colorful and chaotic 4-on-4 action shooter. For the first time, take Turf War battles on-the-go with the Nintendo Switch™ system, and use any of the consoles portable play styles for intense local multiplayer* action. Even team up for new 4-player co-op fun in Salmon Run!'
+        ),
+        ('00000019','Splatoon 2', '37.99', 'STANDARD', 'PRE-OWNED',
+        'Ink-splatting action is back and fresher than ever The squid kids called Inklings are back to splat more ink and claim more turf in this colorful and chaotic 4-on-4 action shooter. For the first time, take Turf War battles on-the-go with the Nintendo Switch™ system, and use any of the consoles portable play styles for intense local multiplayer* action. Even team up for new 4-player co-op fun in Salmon Run!'
+        ),
+        ('00000020','Splatoon 2', '39.99', 'STANDARD', 'DIGITAL',
+        'Ink-splatting action is back and fresher than ever The squid kids called Inklings are back to splat more ink and claim more turf in this colorful and chaotic 4-on-4 action shooter. For the first time, take Turf War battles on-the-go with the Nintendo Switch™ system, and use any of the consoles portable play styles for intense local multiplayer* action. Even team up for new 4-player co-op fun in Salmon Run!'
+        );
+
+INSERT  INTO    STORE
+VALUE
+        ('12345678','00000011','500'),
+        ('92830171','00000002','500'),
+        ('12930839','00000007','230'),
+        ('12930839','00000010','700'),
+        ('12930839','00000001','100'),
+        ('10129832','00000002','301'),
+        ('10129832','00000003','188'),
+        ('12345678','00000020','200'),
+        ('13792610','00000003','102'),
+        ('92830171','00000005','500'),
+        ('12345678','00000019','50'),
+        ('92830171','00000006','200'),
+        ('12345678','00000004','50'),
+        ('13792610','00000008','200'),
+        ('13792610','00000009','300'),
+        ('13792610','00000012','500'),
+        ('92830171','00000013','219'),
+        ('92830171','00000014','438'),
+        ('12345678','00000015','500'),
+        ('12930839','00000016','398'),
+        ('12930839','00000017','40'),
+        ('13792610','00000018','500');
