600bfedf4ab23a0927bf066f49fb0f3ba0ae79d8
Hongtao Huang <18801301060@163.com>
2021-11-26 04:37:30 -0600

    modification according to er diagram

--- create_tables.sql
+++ create_tables.sql
@@ -25,23 +25,23 @@ CREATE TABLE IF NOT EXISTS ITEM
     PRICE       DECIMAL(6,2)    DEFAULT '0.00',
     IEDITION    CHAR(30)        DEFAULT 'STANDARD',
     ICONDITION  CHAR(30)        DEFAULT 'NEW',
-    TAX         DECIMAL(3,2)    DEFAULT '1.15',             --THE DEFAULT TAX WOULD BE 15%
+    TAX         DECIMAL(3,2)    DEFAULT '1.15',             -- THE DEFAULT TAX WOULD BE 15%
     IDETAIL     TEXT                    ,
-    FRESHIP     BOOLEAN         DEFAULT '0',                --DEFAULT=0 MEANS THE BOOLEAN IS FALSE, IT IS NOT A FREE SHIPPING ITEM
-    PROMO       BOOLEAN         DEFAULT '0',                --DEFAULT=0 MEANS USER CANNOT USE COUPON TO BUY THIS ITEM
-    SALE        BOOLEAN         DEFAULT '0',                --MEANS THE ITEM IS NOT ON SALE
-    DISCOUN     DECIMAL(3,2)    DEFAULT '0.00',             --MEANS THE ITEM IS NOT ON SALE
-    TPRICE      DECIMAL(6,2)    DEFAULT '0.00',             --TPRICE WOULD BE (PRICE*TAX-PROMO)*DISCOUNT*SALE-REWARD
-    REWARD      DECIMAL(3,2)    DEFAULT '0.00',             --CAN MAKE A 1% REWARD LIKE GET 1$ REFUND AFTER SPEND 100$ 
+    FRESHIP     BOOLEAN         DEFAULT '0',                -- DEFAULT=0 MEANS THE BOOLEAN IS FALSE, IT IS NOT A FREE SHIPPING ITEM
+    PROMO       BOOLEAN         DEFAULT '0',                -- DEFAULT=0 MEANS USER CANNOT USE COUPON TO BUY THIS ITEM
+    SALE        BOOLEAN         DEFAULT '0',                -- MEANS THE ITEM IS NOT ON SALE
+    DISCOUN     DECIMAL(3,2)    DEFAULT '0.00',             -- MEANS THE ITEM DON'T HAVE DISCOUNT
+    TPRICE      DECIMAL(6,2)    DEFAULT '0.00',             -- TPRICE WOULD BE (PRICE*TAX-PROMO)*DISCOUNT*SALE-REWARD
+    REWARD      DECIMAL(3,2)    DEFAULT '0.00',             -- CAN MAKE A 1% REWARD LIKE GET 1$ REFUND AFTER SPEND 100$ 
 
     PRIMARY KEY (IID)
 );
 
 CREATE TABLE IF NOT EXISTS STORE
 (
-    EMPID       INT(8)          NOT NULL,                   --WHICH EMPLOYEE
-    IID         INT(8)          NOT NULL,                   --PROVIDE WHAT ITEM
-    QTY         INT(8)          DEFAULT '0',                --IN WHAT QUANTITY
+    EMPID       INT(8)          NOT NULL,                   -- WHICH EMPLOYEE
+    IID         INT(8)          NOT NULL,                   -- PROVIDE WHAT ITEM
+    QTY         INT(8)          DEFAULT '0',                -- IN WHAT QUANTITY
 
     PRIMARY KEY (EMPID,IID),
     FOREIGN KEY (EMPID)     REFERENCES HUMAN(ID),
@@ -50,30 +50,32 @@ CREATE TABLE IF NOT EXISTS STORE
 
 CREATE TABLE IF NOT EXISTS PAYMENT
 (
-    UID         INT(8)          NOT NULL,                   --USER ID, DIFFERENT FROM THE EMPLOYEE'S
-    CARDNUM     CHAR(16)        NOT NULL,                   --CARD NUMBER
-    EXPMON      CHAR(2)         NOT NULL,                   --EXPIRE MONTH
-    EXPYEAR     CHAR(4)         NOT NULL,                   --EXPIRE YEAR
-    CVN         CHAR(3)         NOT NULL,                   --SECURITY CODE
-    CFNAME      CHAR(30)        NOT NULL,                   --CARD OWNER'S FIRST NAME 
-    CLNAME      CHAR(30)        NOT NULL,                   --CARD OWNER'S LAST NAME
+    PID         INT(8)          NOT NULL    AUTO_INCREMENT, -- USER CAN CHOOSE THE CARD THEY WANT TO USE
+    UID         INT(8)          NOT NULL,                   -- USER ID, DIFFERENT FROM THE EMPLOYEE'S
+    CARDNUM     CHAR(16)        NOT NULL,                   -- CARD NUMBER
+    EXPMON      CHAR(2)         NOT NULL,                   -- EXPIRE MONTH
+    EXPYEAR     CHAR(4)         NOT NULL,                   -- EXPIRE YEAR
+    CVN         CHAR(3)         NOT NULL,                   -- SECURITY CODE
+    CFNAME      CHAR(30)        NOT NULL,                   -- CARD OWNER'S FIRST NAME 
+    CLNAME      CHAR(30)        NOT NULL,                   -- CARD OWNER'S LAST NAME
 
-    PRIMARY KEY (UID),
+    PRIMARY KEY (PID,UID),
     FOREIGN KEY (UID)       REFERENCES HUMAN(ID)
 );
 
 CREATE TABLE IF NOT EXISTS BILLINGINFO
 (
+    BID         INT(8)          NOT NULL    AUTO_INCREMENT, -- USER CAN CHOOSE THE ADDRESS THEY WANT TO USE
     UID         INT(8)          NOT NULL,
-    ADDRESS     TEXT                    ,                   --USER'S ADDRESS
-    OPTLINE     TEXT                    ,                   --APARTMENT, UNIT, ETC
+    ADDRESS     TEXT                    ,                   -- USER'S ADDRESS
+    OPTLINE     TEXT                    ,                   -- APARTMENT, UNIT, ETC
     CITY        CHAR(50)        NOT NULL,
     STATE       CHAR(10)        NOT NULL,
     ZIPCODE     INT(5)          NOT NULL,
     COUNTRY     CHAR(50)        DEFAULT'USA',
-    CONTACT     CHAR(10)        DEFAULT'PHONE-CALL',        --CONTACT PREFERENCE: TEXT, EMAIL, ETC
+    CONTACT     CHAR(10)        DEFAULT'PHONE-CALL',        -- CONTACT PREFERENCE: TEXT, EMAIL, ETC
 
-    PRIMARY KEY (UID),
+    PRIMARY KEY (BID,UID),
     FOREIGN KEY (UID)       REFERENCES HUMAN(ID)
 );
 
@@ -90,17 +92,16 @@ CREATE TABLE IF NOT EXISTS CART
 
 CREATE TABLE IF NOT EXISTS ORDERSTATUS
 (
-    OID     INT(8)          NOT NULL    AUTO_INCREMENT,     --ORDER ID
-    UID     INT(8)          NOT NULL,                       --WHO CREATE THE ORDER
-    IID     INT(8)          NOT NULL,                       --WHAT ITEM IN THE ORDER
-    EMPID   INT(8)          NOT NULL,                       --WHO PROCESS THE ORDER
-    STATUS  CHAR(10)        DEFAULT 'Pending',              --THE STATUS OF THE ORDER
-    TRACID  INT(8)          AUTO_INCREMENT,                 
-    NOTE    TEXT         ,
+    OID     INT(8)          NOT NULL    AUTO_INCREMENT,     -- ORDER ID
+    UID     INT(8)          NOT NULL,                       -- WHO CREATE THE ORDER
+    IID     INT(8)          NOT NULL,                       -- WHAT ITEM IN THE ORDER
+    EMPID   INT(8)          NOT NULL,                       -- WHO PROCESS THE ORDER
+    STATUS  CHAR(10)        DEFAULT 'Pending',              -- THE STATUS OF THE ORDER
+    NOTE    TEXT                    ,
 
     PRIMARY KEY(OID,UID,IID,EMPID),
      
     FOREIGN KEY (UID)       REFERENCES HUMAN(ID),
     FOREIGN KEY (IID)       REFERENCES ITEM(IID),
-    FOREIGN KEY (EMPID)     REFERENCES HUMAN(ID),
+    FOREIGN KEY (EMPID)     REFERENCES HUMAN(ID)
 );
